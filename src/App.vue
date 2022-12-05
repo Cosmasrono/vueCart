e9c45bc58bfd1393ff2f178071fed271d70686c3
cosmas <cosmas@localhost.localdomain>
2022-12-05 21:04:25 +0300

    add

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+ 
+</script>
+
+<template>
+<main>
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+
+  <RouterView />
+  </main>
+</template>
+
+<style scoped>
+ 
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,45 @@
+<script setup>
+</script>
+
+<template>
+<main>
+<h1>products</h1>
+    <div class=""
+   v-for="product in products"
+  :key="product">{{product.name}}
+  <img :src=" product.image" >
+  <div>{{product.cost}}
+  </div>
+  <button v-on:click="addItemToCart(product)">add to cart
+    </button>
+    </div>
+</main>
+</template>
+<script>
+export default {
+  name:"app",
+  data(){
+    return{
+    products:[
+      {
+        id:'1',
+        name:"cosmas",
+        cost:"500",
+        image:"https://cdn.pixabay.com/photo/2022/11/30/22/19/cottage-7627904__340.jpg"
+      }
+    ]
+
+  }
+
+  },
+  methods:{
+    addItemToCart(product){
+      console.log(product);
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
