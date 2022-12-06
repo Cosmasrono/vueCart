c4fc3b5aa89ebcc409406a0ae33352891f530027
cosmas <cosmas@localhost.localdomain>
2022-12-06 18:21:31 +0300

    add

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,50 @@
-<script setup>
+<script setup> 
 </script>
 
 <template>
-<main>
-<h1>products</h1>
-    <div class=""
-   v-for="product in products"
-  :key="product">{{product.name}}
-  <img :src=" product.image" >
+<main> 
+  <button v-on:click="navigateTo('product')">view product</button>
+  <header>{{cart.length}} products in cart
+    <button v-on:click="navigateTo('cart')">view cart</button>
+  </header>
+  {{pages}}
+
+  <div v-if="page=='cart'">
+    <h1>your cart</h1>
+    <div class="products">
+    <div
+   v-for="product in cart"
+  :key="product"> 
+  <img :src=" product.image" > 
+  {{product.name}}
+  
   <div>{{product.cost}}
   </div>
-  <button v-on:click="addItemToCart(product)">add to cart
+  <button v-on:click="removeItemFromCart(product)" class="p-5 ml-20 text-white rounded-md bg-slate-900">add to cart
     </button>
     </div>
+  </div>
+  </div>
+
+  <div v-if="page=='products'">
+    <h1>products</h1>
+  
+
+  <div class="products">
+    <div
+   v-for="product in products"
+  :key="product"> 
+  <img :src=" product.image" > 
+  {{product.name}}
+  
+  <div>{{product.cost}}
+  </div>
+  <button v-on:click="addItemToCart(product)" class="p-5 ml-20 text-white rounded-md bg-slate-900">remove item
+    </button>
+    </div>
+  </div>
+  </div>
+    
 </main>
 </template>
 <script>
@@ -20,24 +52,52 @@ export default {
   name:"app",
   data(){
     return{
+      page:'products',
+      cart:[],
+      
     products:[
       {
         id:'1',
         name:"cosmas",
         cost:"500",
         image:"https://cdn.pixabay.com/photo/2022/11/30/22/19/cottage-7627904__340.jpg"
-      }
+      },
+      {
+        id:'1',
+        name:"cosmas",
+        cost:"500",
+        image:"https://cdn.pixabay.com/photo/2022/11/30/22/19/cottage-7627904__340.jpg"
+      },
+      {
+        id:'1',
+        name:"cosmas",
+        cost:"500",
+        image:"https://cdn.pixabay.com/photo/2022/11/30/22/19/cottage-7627904__340.jpg"
+      },
+      {
+        id:'1',
+        name:"cosmas",
+        cost:"500",
+        image:"https://cdn.pixabay.com/photo/2022/11/30/22/19/cottage-7627904__340.jpg"
+      },
     ]
 
-  }
+  };
 
   },
   methods:{
     addItemToCart(product){
-      console.log(product);
-    }
+      this.cart.push(product)
+    },
+  navigateTo(page){
+    this.page=page;
   }
-}
+},
+removeItemFromCart(product){
+this.cart.splice(this.cart.indexOf(product))
+},
+components:{}
+};
 </script>
 
 <style>
